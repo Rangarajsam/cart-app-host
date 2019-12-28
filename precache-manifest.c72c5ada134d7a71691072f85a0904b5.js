5417c273f51622a6b39d4ad52b3d3db91e4a71af
RangarajSam <rangaraj.sam@gmail.com>
2019-12-29 01:52:34 +0530

    Host files added

---
+++ precache-manifest.c72c5ada134d7a71691072f85a0904b5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8525e037bd1b65b00fd1c0a5caf2ce52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c6258b417f76aa15f04",
+    "url": "/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "c28ed3c855770f953a9f",
+    "url": "/static/css/main.7c23624b.chunk.css"
+  },
+  {
+    "revision": "6c6258b417f76aa15f04",
+    "url": "/static/js/2.73941bfd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.73941bfd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c28ed3c855770f953a9f",
+    "url": "/static/js/main.3e4b5134.chunk.js"
+  },
+  {
+    "revision": "6a17adbe75ef60371e36",
+    "url": "/static/js/runtime-main.08934e8f.js"
+  },
+  {
+    "revision": "7b311951ac4bb3b5a6828449c7176dc2",
+    "url": "/static/media/loader-icon.7b311951.svg"
+  },
+  {
+    "revision": "06bc6260f5a6c94ea4056c3033588e55",
+    "url": "/static/media/roboto-bold-webfont.06bc6260.ttf"
+  },
+  {
+    "revision": "a5a0afe5bc260afd7491b552d42a6164",
+    "url": "/static/media/roboto-bold-webfont.a5a0afe5.woff"
+  },
+  {
+    "revision": "35ab1505ebbb6753bf20e8bac936d5d1",
+    "url": "/static/media/roboto-light-webfont.35ab1505.ttf"
+  },
+  {
+    "revision": "ccb70e60329b9dc25fbd98165b2c1c15",
+    "url": "/static/media/roboto-light-webfont.ccb70e60.woff"
+  },
+  {
+    "revision": "5bc39b979a583efd980d3c702ca25949",
+    "url": "/static/media/roboto-medium-webfont.5bc39b97.woff"
+  },
+  {
+    "revision": "b3859b2452de71e388be9f983c251786",
+    "url": "/static/media/roboto-medium-webfont.b3859b24.ttf"
+  },
+  {
+    "revision": "5206c89454b8cdc047834eb4edbcdd38",
+    "url": "/static/media/roboto-regular-webfont.5206c894.woff"
+  },
+  {
+    "revision": "7b0cc0ca02cc54bac9833ddf97fc9e00",
+    "url": "/static/media/roboto-regular-webfont.7b0cc0ca.ttf"
+  }
+]);
